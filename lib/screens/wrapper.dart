c7873884d1867b0c87a220e18b08156bb16284a7
yasseennouh <y.h.nouh@student.aast.edu>
2023-11-04 22:43:46 +0200

    bluetooth completely fixed

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_blue_app/screens/Bluetooth/MainPage.dart';
+import 'package:flutter_blue_app/screens/authenticate/sign_in.dart';
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MainPage();
+    return Scene();
   }
 }
